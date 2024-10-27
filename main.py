9962eed9b6f74fa9bfb49c70d8553caa8b63af7f
Stefan Schüttengruber <e11807476@student.tuwien.ac.at>
2024-10-27 16:31:42 +0100

    naaah

--- main.py
+++ main.py
@@ -769,7 +769,7 @@ for input, target in test_dataloader:
     noise_remaining = 10.0 * math.log10(loss_func((output - target), zeros).item())
     output_db = 10.0 * math.log10(loss_func((output), zeros).item())
     target_db = 10.0 * math.log10(loss_func((target), zeros).item())
-    SNR_fac = test_dataloader.get_SNR_fac(idx)
+    SNR_fac = val_data.get_SNR_fac(idx)
     SNR_db = 10.0 * math.log10(SNR_fac / (1.0 - SNR_fac))
 
     fac_noise_red = 10.0 * math.log10(1.0 - SNR_fac)
